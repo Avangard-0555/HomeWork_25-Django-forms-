0b71476dbcee8aa2fccae714b07b7adbf624e51e
Avangard-0555 <turgunovx98@gmail.com>
2024-11-24 08:53:31 +0500

    urls_0.6.py
    Шаг 6
    
    
    Настроить URL-ы для авторизации  В файле users/urls.py добавьте пути к представлениям:

---
+++ urls_0.6.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import register, login_view, logout_view
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+]
+
+Добавьте также ссылки в главный файл URL проекта shop/urls.py:
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),  # добавляем пути для users
+    path('', include('news.urls')),  # Здесь будет основное приложение для новостей
+]
+
+Ш
