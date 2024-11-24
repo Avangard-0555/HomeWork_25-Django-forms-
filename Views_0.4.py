372069dd269187d747b790b231024fdfd8d0273e
Avangard-0555 <turgunovx98@gmail.com>
2024-11-24 08:34:05 +0500

    Create Views_0.4.py
    Шаг (4)
    
    Настроить представления для регистрации, входа и выхода
    
    В файле users/views.py определите функции для регистрации, входа и выхода:

---
+++ Views_0.4.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # На главную страницу после регистрации
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
